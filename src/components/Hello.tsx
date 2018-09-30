5184543178186c63bfb926e8116c74e135007137
Yukitaka Maeda <yumaeda@gmail.com>
2018-09-30 22:16:52 +0900

    Add comment and change double-quote to single quote where possible

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,14 +1,18 @@
-import * as React from "react";
+import * as React from 'react';
 
+/**
+ * HelloProps describes the shape of props
+ */
 export interface HelloProps {
     compiler: string;
     framework: string;
 }
 
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
+/**
+ * State is never set so we use the '{}' type.
+*/
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return <h1>Hello from { this.props.compiler } and { this.props.framework }!</h1>;
     }
 }
