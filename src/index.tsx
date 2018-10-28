1b7175ddc68fb62362abcad0c994c86f989533dc
Yukitaka Maeda <yumaeda@gmail.com>
2018-10-28 19:35:43 +0900

    Enable redux-devtools-extension

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,17 @@
  */
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { applyMiddleware, createStore } from 'redux'
+import { applyMiddleware, compose, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga'
 import { Root } from './components/Root'
 import rootReducer from './reducers'
 import rootSaga from './sagas'
 
 const sagaMiddleware = createSagaMiddleware()
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+const store = createStore(rootReducer, composeWithDevTools(
+    applyMiddleware(sagaMiddleware)
+))
 sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(
