e65fc4fbc11ee4100023fdf14c096e7339bf1cbd
Yukitaka Maeda <yumaeda@gmail.com>
2018-11-07 20:26:55 +0900

    Add CartUtility.ts

---
+++ src/lib/CartUtility.ts
@@ -0,0 +1,49 @@
+/**
+ * CartUtility (Singleton)
+ *
+ * @author Yukitaka Maeda [yumaeda@gmail.com]
+ */
+
+/**
+ * Interface for CartItem
+ */
+interface ICartItem {
+    apply: string
+    availability: string
+    etc: string
+    price: number
+    stock: number
+}
+
+/**
+ * Gets an boolean value whether the specified item is purchasable or not
+ *
+ * @param ICartItem item to check
+ * @return boolean
+ */
+export function isPurchasable(item: ICartItem): boolean {
+    return (
+        (item.apply !== 'SO') &&
+        (item.apply !== 'DP') &&
+        (item.availability === 'Online') &&
+        (item.stock > 0) &&
+        (item.price > 0)
+    )
+}
+
+/**
+ * Gets an boolean value whether the specified item will be coming soon or not
+ *
+ * @param ICartItem item to check
+ * @return boolean
+ */
+export function isComingSoon(item: ICartItem): boolean {
+    const { availability, etc, stock } = item
+    const regexp = new RegExp(/^\d{1,2}\.\d{1,2}$/)
+
+    if (!etc || !regexp.test(etc)) {
+        return false
+    }
+
+    return (stock > 0) && (availability === 'Online')
+}
